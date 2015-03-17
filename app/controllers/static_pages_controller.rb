eadb25030436497674324ae2e02658818b9b4e66
Michael Pierce <michael@affektive.com>
2015-03-16 18:20:27 -0600

    Latest code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,22 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def index
+  end
+
+  def services
+  end
+
+  def booking
+  end
+
+  def location
+  end
+
+  def highlights
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ HairTrendz::Application.routes.draw do
   root 'static_pages#index'
 
   match '/index', to: 'static_pages#index', via: 'get'
-
+  match '/services', to: 'static_pages#services', via: 'get'
+  match '/booking', to: 'static_pages#booking', via: 'get'
+  match '/location', to: 'static_pages#location', via: 'get'
+  match '/highlights', to: 'static_pages#highlights', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
 end
